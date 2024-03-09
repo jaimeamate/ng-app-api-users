f05bd4b5da5ff716c57f89bfb66ba35e2deb8fdb
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-03-09 12:15:51 +0100

    basics components & pages created

---
+++ src/app/pages/user-view/user-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-view.component.html',
+  styleUrl: './user-view.component.css'
+})
+export class UserViewComponent {
+
+}
