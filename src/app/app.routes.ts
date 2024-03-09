289ebb86376d50ada370e53f65d9bfa5475f3da7
Jaime Amate R2D <jaime_amate@roadtodata.com>
2024-03-09 12:25:58 +0100

    routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './pages/users-list/users-list.component';
+import { UserViewComponent } from './pages/user-view/user-view.component';
+import { UserFormComponent } from './pages/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        pathMatch:'full',
+        redirectTo:'home'
+    },
+    {
+        path:'home',
+        component:UsersListComponent
+    },
+    {
+        path:'user/:id', 
+        component:UserViewComponent
+    },
+    {
+        path:'newuser', 
+        component:UserFormComponent
+    },
+    {
+        path:'updateuser/:id', 
+        component:UserFormComponent
+    },
+    {
+        path:'**', 
+        component:UsersListComponent
+    }
+];
